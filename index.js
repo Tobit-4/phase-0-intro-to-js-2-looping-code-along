37d12750b0b3eb49dfc2df19519f68f5d32ea04d
Fidel <fidel.kipkoech@student.moringaschool.com>
2025-03-04 21:31:30 +0300

    made changes

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    let messages = [];
+
+    for(let i=0; i < names.length; i++){
+         messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return messages;
+}
+const namesArray = ["Guadalupe", "Ollie", "Aki"]
+console.log(writeCards(namesArray,"birthday"));
+function countDown(number){
+    while (number >= 0){
+        console.log(number);
+        number--;
+        
+    }
+}
+countDown(10)
